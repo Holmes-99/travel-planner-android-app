e187fe2f946b73b28558e384656b3390d0eba65f
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-12 19:38:56 +0300

    addTrip updateTrip and some other bugs

--- src/main/java/com/example/travelplanner/services/TripService.java
+++ src/main/java/com/example/travelplanner/services/TripService.java
@@ -11,18 +11,32 @@ public class TripService {
 
     @Autowired
     TripRepository tripRepository;
-    public List<Trip> getAllTrips(){
+
+    public List<Trip> getAllTrips() {
         return tripRepository.findAll();
     }
 
-    public List <Trip> getTopRatedTrips(){
-        List<Trip> all =tripRepository.findAll();
-        for (int i =0 ;i<all.size() ; i++){
-            if(all.get(i).getRating() !=null && all.get(i).getRating()>= 4.5)
+    public List<Trip> getTopRatedTrips() {
+        List<Trip> all = tripRepository.findAll();
 
-        List <Trip> topRated = new ArrayList<Trip>();
-            topRated.add(all.get(i));
+        List<Trip> topRated = new ArrayList<Trip>();
+
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).getRating() != null && all.get(i).getRating() >= 4.5) {
+                topRated.add(all.get(i));
+            }
         }
-        }
+        return topRated;
+    }
+
+    public Trip addTrip(Trip trip){
+    return tripRepository.save(trip);
+    }
+
+    public Trip update Trip(Trip trip , Integer id){
+       trip.setId(id);
+       return tripRepository.save(trip);
     }
 }
+
+
