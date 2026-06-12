c27a4ffac6496de5c29eeab1d85c66989d9214e6
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-12 19:54:32 +0300

    add UserService methods (add-get-delete)

--- src/main/java/com/example/travelplanner/services/UserService.java
+++ src/main/java/com/example/travelplanner/services/UserService.java
@@ -1,6 +1,33 @@
 package com.example.travelplanner.services;
 
+import com.example.travelplanner.models.User;
+import com.example.travelplanner.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User addUser (User user){
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user , Integer id){
+        user.setId(id);
+    return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id){
+        userRepository.deleteById(id);
+    }
 
 
 }
