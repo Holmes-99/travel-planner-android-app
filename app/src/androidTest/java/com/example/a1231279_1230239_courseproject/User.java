31802e301bf005baa2dc543fe14ec38ea8baef25
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-14 20:04:53 +0300

    add the user class

---
+++ app/src/androidTest/java/com/example/a1231279_1230239_courseproject/User.java
@@ -0,0 +1,114 @@
+package com.example.a1231279_1230239_courseproject;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String major;
+    private String phone;
+    private String email;
+    private String password;
+    private String gender;
+    private String role ;
+    private String profilepic;
+
+    public User(){}
+
+    public User(int id, String firstName,
+                String lastName, String major,
+                String phone, String email,
+                String password, String gender,
+                String role, String profilepic) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.role = role;
+        this.profilepic = profilepic;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getprofilepic() {
+        return profilepic;
+    }
+
+    public void setprofilepic(String profilepic) {
+        this.profilepic = profilepic;
+    }
+}
