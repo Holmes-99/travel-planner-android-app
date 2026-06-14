1c20125068b17234fa01d0b9ab70ab4888d5198a
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-14 16:10:14 +0300

    create UserController and its endpoints and correct some flaws in other files

--- src/main/java/com/example/travelplanner/controllers/TripController.java
+++ src/main/java/com/example/travelplanner/controllers/TripController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 public class TripController {
 
@@ -15,28 +16,32 @@ public class TripController {
 TripService tripService;
 
     @GetMapping("/trips")
+
     public List<Trip> getAllTrips(){
-
-        return tripService.getAllTrips();
+    return tripService.getAllTrips();
 }
-
     @GetMapping("/trips/top-rated")
+
     public List<Trip> getTopRatedTrips() {
-        return tripService.getTopRatedTrips();
+       return tripService.getTopRatedTrips();
     }
+
 
     @PostMapping("/trips")
+
     public Trip addTrip(@RequestBody Trip trip) {
-        return tripService.addTrip(trip);
-    }
+      return tripService.addTrip(trip);
+     }
 
     @PutMapping("/trips/{id}")
+
     public Trip updateTrip(@PathVariable Integer id, @RequestBody Trip trip) {
-        return tripService.updateTrip(trip, id);
+        return tripService.updateTrip(trip,id);
     }
 
     @DeleteMapping("/trips/{id}")
+
     public void deleteTrip(@PathVariable Integer id) {
-        tripService.deleteTrip(id);
-    }
+        tripService.deleteTrip(id); }
+
 }
