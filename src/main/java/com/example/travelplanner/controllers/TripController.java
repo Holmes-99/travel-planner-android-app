cbdfb23d3319876a60f1761a6be73f1bebc03ce1
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-14 14:33:32 +0300

    trip controller other methods

--- src/main/java/com/example/travelplanner/controllers/TripController.java
+++ src/main/java/com/example/travelplanner/controllers/TripController.java
@@ -4,20 +4,39 @@ package com.example.travelplanner.controllers;
 import com.example.travelplanner.models.Trip;
 import com.example.travelplanner.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TripController {
+
 @Autowired
 TripService tripService;
-@GetMapping("/trip")
+
+    @GetMapping("/trip")
     public List<Trip> getAllTrips(){
-    return tripService.getAllTrips();
+
+        return tripService.getAllTrips();
 }
 
+    @GetMapping("/trips/top-rated")
+    public List<Trip> getTopRatedTrips() {
+        return tripService.getTopRatedTrips();
+    }
+
+    @PostMapping("/trips")
+    public Trip addTrip(@RequestBody Trip trip) {
+        return tripService.addTrip(trip);
+    }
+
+    @PutMapping("/trips/{id}")
+    public Trip updateTrip(@PathVariable Integer id, @RequestBody Trip trip) {
+        return tripService.updateTrip(trip, id);
+    }
+
+    @DeleteMapping("/trips/{id}")
+    public void deleteTrip(@PathVariable Integer id) {
+        tripService.deleteTrip(id);
+    }
 }
