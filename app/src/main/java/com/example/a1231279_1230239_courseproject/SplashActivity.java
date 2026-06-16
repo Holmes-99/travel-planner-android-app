b85a8bbc43e3e08d1de3f0887527ecf634e294c7
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-16 19:27:42 +0300

    create the splash activity

---
+++ app/src/main/java/com/example/a1231279_1230239_courseproject/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.a1231279_1230239_courseproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView_logo = findViewById(R.id.imageView_logo);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade);
+        imageView_logo.startAnimation(anim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+            }
+        }
