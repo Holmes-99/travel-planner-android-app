4bf87e42bce6508652dd13492ab390a00c5a38fb
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-16 16:20:41 +0300

    add the trip json parser

---
+++ app/src/androidTest/java/com/example/a1231279_1230239_courseproject/TripJsonParser.java
@@ -0,0 +1,37 @@
+package com.example.a1231279_1230239_courseproject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripJsonParser {
+
+    public static List<Trip> getTripsFromJson(String json) {
+        List<Trip> trips = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+        for (int i=0; i<jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            Trip trip = new Trip();
+            trip.setId(jsonObject.getInt("id"));
+            trip.setCountry(jsonObject.getString("country"));
+            trip.setDestination(jsonObject.getString("destination"));
+            trip.setDurationDays(jsonObject.getInt("durationDays"));
+            trip.setPrice(jsonObject.getDouble("price"));
+            trip.setRating(jsonObject.getDouble("rating"));
+            trip.setDescription(jsonObject.getString("description"));
+            trip.setImage(jsonObject.getString("image"));
+            trips.add(trip);
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return trips;
+
+
+        }
+    }
+
