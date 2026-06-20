963d1f1c15bcde7c9b9cb84ea294fb6c24a92d7d
Holmes-99 <abualrubshatha1@gmail.com>
2026-06-20 19:48:07 +0300

    create register activity

--- app/src/main/java/com/example/a1231279_1230239_courseproject/RegisterActivity.java
+++ app/src/main/java/com/example/a1231279_1230239_courseproject/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.a1231279_1230239_courseproject;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,23 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        EditText editTextFirstName = findViewById(R.id.editText_firstName);
+        EditText editTextLastName = findViewById(R.id.editText_lastName);
+        EditText editTextEmail = findViewById(R.id.editText_Remail);
+        EditText editTextPassword = findViewById(R.id.editText_Rpassword);
+        EditText editTextConfirmPassword = findViewById(R.id.editText_confirmPassword);
+        EditText editTextPhone = findViewById(R.id.editText_phone);
+        Spinner spinnerGender = findViewById(R.id.spinner_gender);
+        Spinner spinnerMajor = findViewById(R.id.spinner_Rmajor);
+        Button buttonRegister = findViewById(R.id.button_register);
+
+        //major
+        String[] majorArray = getResources().getStringArray(R.array.major);
+        ArrayAdapter<String> majorAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, majorArray);
+        majorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMajor.setAdapter(majorAdapter);
+
+
     }
 }
