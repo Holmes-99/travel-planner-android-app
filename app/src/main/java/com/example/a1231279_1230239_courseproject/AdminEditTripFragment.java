ff0bfb4987d5d0fcde94fcd33c97c023f69cfcd7
LaraDaifallah <lara.daifallah.23@gmail.com>
2026-06-21 22:11:00 +0300

    Adding edit trip fragment

---
+++ app/src/main/java/com/example/a1231279_1230239_courseproject/AdminEditTripFragment.java
@@ -0,0 +1,24 @@
+package com.example.a1231279_1230239_courseproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdminEditTripFragment extends Fragment {
+
+    public AdminEditTripFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_edit_trip, container, false);
+    }
+}
