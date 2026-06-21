8c506f11f6fd890cc3e72c4443bea013af1a4a58
LaraDaifallah <lara.daifallah.23@gmail.com>
2026-06-21 14:05:39 +0300

    Profile fragment

--- app/src/main/java/com/example/a1231279_1230239_courseproject/ProfileFragment.java
+++ app/src/main/java/com/example/a1231279_1230239_courseproject/ProfileFragment.java
@@ -1,64 +1,176 @@
 package com.example.a1231279_1230239_courseproject;
 
+import static androidx.activity.result.ActivityResultCallerKt.registerForActivityResult;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ProfileFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
+@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+    EditText editTextFirstName, editTextLastName, editTextPhone;
+    EditText editTextPassword, editTextConfirmPassword;
+    Button buttonUpdate, buttonPickPhoto;
+    ImageView imageViewProfile;
+    String profilePicUri = "";
+    String currentMajor = "";
+
+    private String encryptPassword(String password) {
+        try {
+            java.security.MessageDigest md =
+                    java.security.MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                hexString.append(String.format("%02X", b));
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            return password;
+        }
+    }
+
+    ActivityResultLauncher<Intent> imagePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK &&
+                        result.getData() != null) {
+                    Uri selectedImage = result.getData().getData();
+                    profilePicUri = selectedImage.toString();
+                    imageViewProfile.setImageURI(selectedImage);
+                }
+            });
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editTextFirstName = view.findViewById(R.id.editText_profileFirstName);
+        editTextLastName = view.findViewById(R.id.editText_profileLastName);
+        editTextPhone = view.findViewById(R.id.editText_profilePhone);
+        editTextPassword = view.findViewById(R.id.editText_profilePassword);
+        editTextConfirmPassword = view.findViewById(R.id.editText_profileConfirmPassword);
+        buttonUpdate = view.findViewById(R.id.button_updateProfile);
+        buttonPickPhoto = view.findViewById(R.id.button_pickPhoto);
+        imageViewProfile = view.findViewById(R.id.imageView_profile);
+
+
+        SharedPreManager prefs = SharedPreManager.getInstance(getActivity());
+        int userId = prefs.readInt("loggedInUserId", -1);
+
+        DataBaseHelper db = new DataBaseHelper(
+                getActivity(), "TravelPlanner.db", null, 1);
+        //load user data
+        String email = prefs.readString("LoggedInEmail", "");
+        Cursor cursor = db.getUserByEmail(email);
+        if (cursor != null && cursor.moveToFirst()) {
+            editTextFirstName.setText(cursor.getString(
+                    cursor.getColumnIndexOrThrow("FIRSTNAME")));
+            editTextLastName.setText(cursor.getString(
+                    cursor.getColumnIndexOrThrow("LASTNAME")));
+            editTextPhone.setText(cursor.getString(
+                    cursor.getColumnIndexOrThrow("PHONE")));
+            String pic = cursor.getString(cursor.getColumnIndexOrThrow("PROFILEPIC"));
+            if (pic != null && !pic.isEmpty()) {
+                imageViewProfile.setImageURI(Uri.parse(pic));
+                profilePicUri = pic;
+            }
+            cursor.close();
+        }
+        buttonPickPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                imagePickerLauncher.launch(intent);
+            }
+        });
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = editTextFirstName.getText().toString().trim();
+                String lastName = editTextLastName.getText().toString().trim();
+                String phone = editTextPhone.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+                if (firstName.isEmpty() || lastName.isEmpty() || phone.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (firstName.length() < 3 || lastName.length() < 3) {
+                    Toast.makeText(getActivity(),
+                            "Name must be at least 3 characters", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Cursor c= db.getUserByEmail(email);
+                String currentPassword="";
+                String currentGender="";
+                String currentMajor="";
+                if(c!= null && c.moveToFirst()){
+                    currentPassword= c.getString(c.getColumnIndexOrThrow("PASSWORD"));
+                    currentGender= c.getString(c.getColumnIndexOrThrow("GENDER"));
+                    currentMajor= c.getString(c.getColumnIndexOrThrow("MAJOR"));
+                    c.close();
+                }
+                String finalPassword=currentPassword;
+                if(!password.isEmpty()){
+                    if(password.length()<6 || !password.matches(".*[a-zA-Z].*")||!password.matches(".*[0-9].*")){
+                        Toast.makeText(getActivity(),"Password must be at least 6 chars with letters and numbers",Toast.LENGTH_SHORT).show();
+                        return;
+
+                    }
+                    if (!password.equals(confirmPassword)){
+                        Toast.makeText(getActivity(),
+                                "Passwords do not match",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    finalPassword= encryptPassword(password);
+                }
+                User user= new User();
+                user.setId(userId);
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                user.setPhone(phone);
+                user.setEmail(email);
+                user.setGender(currentGender);
+                user.setPassword(finalPassword);
+                user.setprofilepic(profilePicUri);
+
+                int result= db.updateUser(user);
+                if (result >0){
+                    prefs.writeString("LoggedInFirstName",firstName);
+                    Toast.makeText(getActivity(), "Profile updated!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+                }
+        });
+
+
+    }
+
+
 }
